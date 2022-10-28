f19a48b2f84b220df86536196344699b40279017
Edouard Eltherington <edouard.eltherington@gmail.com>
2022-10-27 22:30:06 -0700

    cannot excute binary file error holds

--- docs/research/rhubarb_test/build/index.js
+++ docs/research/rhubarb_test/build/index.js
@@ -1,10 +1,37 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var node_child_process_1 = require("node:child_process");
+var node_process_1 = require("node:process");
 console.log("Start Rhubard....");
-(0, node_child_process_1.exec)('"./rhubarb/rhubarb.exe" ./rhubarb -o output.txt rhubarbtest.wav', function (error, stdout, stderr) {
+console.log("Starting directory: $".concat((0, node_process_1.cwd)()));
+(0, node_process_1.cwd)();
+try {
+    (0, node_process_1.chdir)('../rhubarb');
+    console.log("New directory: ".concat((0, node_process_1.cwd)()));
+}
+catch (err) {
+    console.log("Error message: ".concat(err));
+}
+(0, node_child_process_1.exec)("chmod a+x ./rhubarb.exe", function (error, stout) {
     if (error) {
         throw error;
     }
-    console.log(stdout);
+    else {
+        console.log(node_process_1.stdout);
+    }
 });
+(0, node_process_1.cwd)();
+(0, node_child_process_1.exec)('"./rhubarb.exe" -o output.txt en-Amber.wav', function (error, stdout, stderr) {
+    if (error) {
+        throw error;
+    }
+    else {
+        console.log(stdout);
+    }
+});
+// //  exec('"./rhubarb.exe" ./rhubarb -o output.txt en-Amber.wav', (error,stdout,stderr) => {
+//      if (error) {
+//             throw error;
+//        }
+//       console.log(stdout);
+//      });
--- docs/research/rhubarb_test/src/index.ts
+++ docs/research/rhubarb_test/src/index.ts
@@ -1,14 +1,41 @@
 import { exec } from 'node:child_process'
+import { stdout, chdir, cwd } from 'node:process';
 
 console.log("Start Rhubard....")
 
+console.log(`Starting directory: $${cwd()}`)
 
-exec('"./rhubarb/rhubarb.exe" ./rhubarb -o output.txt rhubarbtest.wav', (error,stdout,stderr) => {
+cwd()
+
+try {
+    chdir('../rhubarb')
+    console.log(`New directory: ${cwd()}`)
+} catch(err) {
+    console.log(`Error message: ${err}`)
+}
+
+exec("chmod a+x ./rhubarb.exe", (error,stout) => {
     if (error) {
-            throw error;
-       }
-       console.log(stdout);
-    });
+        throw error } else {
+            console.log(stdout)
+        } 
+})
+
+cwd()
+
+exec('"./rhubarb.exe" -o output.txt en-Amber.wav', (error,stdout,stderr) => {
+      if (error) {
+        throw error;
+       } else {
+      console.log(stdout);
+     }});
+
+// //  exec('"./rhubarb.exe" ./rhubarb -o output.txt en-Amber.wav', (error,stdout,stderr) => {
+//      if (error) {
+//             throw error;
+//        }
+//       console.log(stdout);
+//      });
 
 
 
