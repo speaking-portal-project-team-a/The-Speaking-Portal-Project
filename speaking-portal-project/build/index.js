cc0b339e5d91dd7015fc384944dcb90dfa57868f
Edouard Eltherington <edouard.eltherington@gmail.com>
2022-11-03 20:56:55 -0700

    added an aysch call and output.json content extraction

--- speaking-portal-project/build/index.js
+++ speaking-portal-project/build/index.js
@@ -1,25 +1,72 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var node_child_process_1 = require("node:child_process");
-var node_process_1 = require("node:process");
-var rhubarbProcessor = function (output_file_name, audio_file_name, text_file_name) {
-    console.log("Starting Rhubarb....");
-    console.log("Starting directory: $".concat((0, node_process_1.cwd)()));
-    try {
-        (0, node_process_1.chdir)('./rhubarb');
-        console.log("New directory: ".concat((0, node_process_1.cwd)()));
-    }
-    catch (err) {
-        console.log("Error message: ".concat(err));
-    }
-    (0, node_child_process_1.exec)("\"./rhubarb\" -o output.json --exportFormat json -r pocketSphinx -d ".concat(text_file_name, " --extendedShapes GX ").concat(audio_file_name), function (error, stdout, stderr) {
-        if (error) {
-            throw error;
-        }
-        else {
-            console.log(stdout);
-        }
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-rhubarbProcessor('output.txt', 'en-Amber.wav', 'en-text.txt');
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var node_process_1 = require("node:process");
+var node_util_1 = __importDefault(require("node:util"));
+var fs_1 = __importDefault(require("fs"));
+var exec = node_util_1.default.promisify(require('node:child_process').exec);
+function rhubarbProcessor(audio_file_name, text_file_name) {
+    return __awaiter(this, void 0, void 0, function () {
+        var _a, stout, stderr, data, mouthCues;
+        return __generator(this, function (_b) {
+            switch (_b.label) {
+                case 0:
+                    console.log("Starting Rhubarb....");
+                    console.log("Starting directory: $".concat((0, node_process_1.cwd)()));
+                    try {
+                        (0, node_process_1.chdir)('./rhubarb');
+                        console.log("New directory: ".concat((0, node_process_1.cwd)()));
+                    }
+                    catch (err) {
+                        console.log("Error message: ".concat(err));
+                    }
+                    return [4, exec("\"./rhubarb\" -o output.json --exportFormat json -r pocketSphinx -d ".concat(text_file_name, " --extendedShapes GX ").concat(audio_file_name))];
+                case 1:
+                    _a = _b.sent(), stout = _a.stout, stderr = _a.stderr;
+                    data = fs_1.default.readFileSync('output.json', 'utf8');
+                    mouthCues = JSON.parse(data);
+                    return [2];
+            }
+        });
+    });
+}
+rhubarbProcessor('en-Amber.wav', 'en-text.txt');
 //# sourceMappingURL=index.js.map
--- speaking-portal-project/src/index.tsx
+++ speaking-portal-project/src/index.tsx
@@ -1,12 +1,15 @@
-import { exec } from 'node:child_process'
-import { stdout, chdir, cwd } from 'node:process';
+import { stdout, chdir, cwd } from 'node:process'
+import util from 'node:util'
+import fs from 'fs'
 
-const rhubarbProcessor = (output_file_name : string, audio_file_name : string, text_file_name ? : string ) => {
+const exec = util.promisify(require('node:child_process').exec)
+
+async function rhubarbProcessor (audio_file_name : string, text_file_name ? : string ) {
     
     console.log("Starting Rhubarb....")
     
     console.log(`Starting directory: $${cwd()}`)
-
+// need child process verbose ? check out -> https://stackoverflow.com/questions/14332721/node-js-spawn-child-process-and-get-terminal-output-live
     try {
         chdir('./rhubarb')
         console.log(`New directory: ${cwd()}`)
@@ -14,16 +17,14 @@ const rhubarbProcessor = (output_file_name : string, audio_file_name : string, t
         console.log(`Error message: ${err}`)
     }
 
-exec(`"./rhubarb" -o output.json --exportFormat json -r pocketSphinx -d ${text_file_name} --extendedShapes GX ${audio_file_name}`, (error,stdout,stderr) => {
-    if (error) {
-      throw error;
-     } else {
-    console.log(stdout);
-   }});
+    const {stout, stderr} = await exec(`"./rhubarb" -o output.json --exportFormat json -r pocketSphinx -d ${text_file_name} --extendedShapes GX ${audio_file_name}`)
+
+    const data = fs.readFileSync('output.json', 'utf8')
+
+    const mouthCues = JSON.parse(data)
 
 }
-
-rhubarbProcessor('output.txt','en-Amber.wav','en-text.txt')
+    rhubarbProcessor('en-Amber.wav','en-text.txt')
 
 
 
