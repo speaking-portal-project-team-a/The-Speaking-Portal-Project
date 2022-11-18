1ffabf60b425ce3dcd9fa26dcc6f8011016d3482
Veronica Jack <veronica.jack73@gmail.com>
2022-11-18 10:47:24 -0800

    handles exception for phoneme factory input files

--- speaking-portal-project/src/index.ts
+++ speaking-portal-project/src/index.ts
@@ -1,13 +1,31 @@
-import { rhubarbProcessor} from "./phonemeFactory/rhubarb"
+import { chdir, cwd, kill } from 'node:process'
+import { rhubarbProcessor } from "./phonemeFactory/rhubarb"
+import { checkFiles } from "./test/file_check"
 
-
+// Main Function
 async function main () {
 
-    console.log("Running Rhubarb Processor....")
+    // Received files will be saved in the Rhubarb directory
+    // The following variables will also be received:
+    const audio_file_name:string = 'en-Amber.wav'
+    const text_file_name:string = 'en-text.txt'
+    const selected_language:string = 'English (U.S.)'
 
-    const phonemeContents = await rhubarbProcessor('en-Amber.wav','en-text.txt')
+    try{
+        // TODO: Check the files FIRST, THEN run Rhubarb
+        console.log(`Validating Files...`)
+        await checkFiles(audio_file_name, text_file_name)
 
-    console.log(phonemeContents)
+        console.log("Running Rhubarb Processor...")
+        const phonemeContents = await rhubarbProcessor(selected_language, audio_file_name, text_file_name)
+
+        console.log("Printing phoneme timings from json file...")
+        console.log(phonemeContents)
+
+    } catch (err: any) {
+        console.log(`Error message: ${err}`)
+    }
 
 }
+
 main()
--- speaking-portal-project/src/phonemeFactory/rhubarb.ts
+++ speaking-portal-project/src/phonemeFactory/rhubarb.ts
@@ -5,17 +5,25 @@ import { MouthCue, MouthCueArray } from '../types'
 
 const exec = util.promisify(require('node:child_process').exec)
 
-export async function rhubarbProcessor (audio_file_name : string, text_file_name ? : string ) {
-     
+// Rhubarb Processor takes audio and text file, produces json file with phoneme timings
+export async function rhubarbProcessor (selected_language : string, audio_file_name : string, text_file_name : string ) {
     // Need child process verbose ? check out -> https://stackoverflow.com/questions/14332721/node-js-spawn-child-process-and-get-terminal-output-live
-    try {
-        chdir('./rhubarb')
-    } catch(err) {
-        console.log(`Error message: ${err}`)
-    }
+
+    // Commented this out because I changed the directory in src/test/file_check.ts
+    // try {
+    //     chdir('./rhubarb')
+    // } catch(err) {
+    //     console.log(`Error message: ${err}`)
+    // }
+
+    // Determine which recognizer to use based on language (English or non-English)
+    var recognizer:string;
+    selected_language.toLowerCase().includes('english') ? recognizer = 'pocketSphinx' : recognizer = 'phonetic'
 
     // Run Rhubarb child process
-    const {stderr} = await  exec(`"./rhubarb" -o output.json --exportFormat json -r pocketSphinx -d ${text_file_name} --extendedShapes GX ${audio_file_name}`)
+    const rhubarbCommand:string = `"./rhubarb" -o output.json --exportFormat json -r ${recognizer} -d ${text_file_name} --extendedShapes GX ${audio_file_name}`
+    console.log(`Running command: + ${rhubarbCommand}`)
+    const {stderr} = await exec(rhubarbCommand)
 
     if (stderr) {
         console.log(stderr)
