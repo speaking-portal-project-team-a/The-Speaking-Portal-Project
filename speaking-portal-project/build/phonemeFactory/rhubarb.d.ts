350d75bdd2d30cbf98fe85c95085a2dfac2459b5
Veronica Jack <veronica.jack73@gmail.com>
2022-11-18 18:09:25 -0800

    Revert "create Nov. 18 minutes and branch update"
    This reverts commit 88c080a48691b7ebe82c90897065f6a3adae620b.

--- speaking-portal-project/build/phonemeFactory/rhubarb.d.ts
+++
@@ -1,2 +0,0 @@
-import { MouthCue } from '../types';
-export declare function rhubarbProcessor(selected_language: string, audio_file_name: string, text_file_name: string): Promise<MouthCue[]>;
--- speaking-portal-project/build/test/file_check.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function checkFiles(audio_file_name: string, text_file_name: string): Promise<void>;
-export declare function checkFileExistence(file: string): void;
-export declare function checkFileReadAccess(file: string): void;
-export declare function checkWavFile(file: string): void;
-export declare function checkTextFile(file: string): void;
