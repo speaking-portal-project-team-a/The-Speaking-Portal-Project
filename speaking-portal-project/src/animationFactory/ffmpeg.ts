6fa011365801469bfa2e70aeb07b04970fd66af8
Edouard Eltherington <edouard.eltherington@gmail.com>
2022-11-23 15:16:07 -0800

    quicktime player error fix

--- speaking-portal-project/src/animationFactory/ffmpeg.ts
+++ speaking-portal-project/src/animationFactory/ffmpeg.ts
@@ -14,7 +14,7 @@ export async function ffmpegProcessor(audio_file_name: string, text_file_name: s
   console.log(cwd())
   // Run ffmpeg child process
   // Warning: Command may not be correct for audio input
-  const { stderr } = await exec(`ffmpeg -f concat -i ${text_file_name} -i ${audio_file_name} -r 24 output.mp4`)
+  const { stderr } = await exec(`ffmpeg -f concat -i ${text_file_name} -i ${audio_file_name} -r 24 -pix_fmt yuv420p output.mp4`)
 
   if (stderr) {
     console.log(stderr)
