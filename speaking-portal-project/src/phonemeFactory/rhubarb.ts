7cd4cf5e3f96b7907ecdbde2125919fff0b8efd1
mkuelker <61034256+mkuelker@users.noreply.github.com>
2023-03-10 22:13:44 -0800

    add change

--- speaking-portal-project/src/phonemeFactory/rhubarb.ts
+++ speaking-portal-project/src/phonemeFactory/rhubarb.ts
@@ -39,7 +39,7 @@ export async function rhubarbProcessor(
         '--extendedShapes',
         'GHX',
         `.${audio_file_name}`,
-        '--threads 8',
+        
     ]
 
     const rhubarbProc = spawnSync('./rhubarb', args)
